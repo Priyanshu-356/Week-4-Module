768be0bf474f2b193bdcd7413ae13f6f33ccf234
Priyanshu Kumar Bhaiya <priyanshu.kr356@gmail.com>
2026-07-24 00:46:23 +0530

    Add files via upload

---
+++ Week 4 Module/Sum_N_natural.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    long long sum = 0;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        sum = sum + i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
